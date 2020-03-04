cf3728809daab49437b4ade4a354fbd75158073f
DenisdS <denis_santos1989@hotmail.com>
2020-03-04 00:52:30 -0300

    Create test for header component

---
+++ client/src/components/header/header.spec.js
@@ -0,0 +1,14 @@
+import renderer from 'react-test-renderer';
+import React from 'react';
+import Header from './header'
+
+it('Verify if header renders correctly', () => {
+  const tree = renderer
+    .create(
+      <Header>
+        <h1><em></em></h1>
+        <p></p>
+      </Header>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
