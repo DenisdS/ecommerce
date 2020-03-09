e62cd2ad6853da23b2cdd571580cfb823c921586
DenisdS <denis_santos1989@hotmail.com>
2020-03-09 12:42:40 -0300

    Add styled-components and normalize

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import { createGlobalStyle }  from 'styled-components'
+import { normalize } from 'styled-normalize'
 
 import Menu from '../components/menu'
 import Routes from '../routes/routes'
 import Header from '../components/header'
 
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+  a{
+    text-decoration: none;
+  }
+  li{
+    list-style: none;
+  }
+`
+
 const App = () => {
   return(
     <>
+      <GlobalStyle />
       <Header />
       <Menu />
       <Routes />
